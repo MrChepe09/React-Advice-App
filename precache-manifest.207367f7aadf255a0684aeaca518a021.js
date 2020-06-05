0b98c21a2bfbadc0d2b94b3bac9ad5f306a368ff
Bhupinder Singh <khanujabhupinder09@gmail.com>
2020-06-05 18:43:00 +0530

    Advice app added

---
+++ precache-manifest.207367f7aadf255a0684aeaca518a021.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2db60a0d475e7cfcce1a88f42e657af3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a0dd1911eff929e75530",
+    "url": "/static/css/main.a1f19212.chunk.css"
+  },
+  {
+    "revision": "6a69037d55b7b66eb92e",
+    "url": "/static/js/2.5bb71376.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5bb71376.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0dd1911eff929e75530",
+    "url": "/static/js/main.ace6d3e7.chunk.js"
+  },
+  {
+    "revision": "c352d36a5a879648b651",
+    "url": "/static/js/runtime-main.45832bfc.js"
+  },
+  {
+    "revision": "91c109fc2e2a14e1e0a445d4314f9334",
+    "url": "/static/media/city.91c109fc.jpg"
+  }
+]);
